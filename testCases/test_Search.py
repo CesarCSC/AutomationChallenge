509e40e7e6da339bd5488a67f819d4bdb31c1db9
Cesar Jacinto <cesar.jacinto@leveltenenergy.com>
2024-06-19 12:44:03 -0600

    Removed unused code

--- testCases/test_Search.py
+++ testCases/test_Search.py
@@ -34,60 +34,6 @@ class TestSearch(unittest.TestCase):
         homepage.select_listed_item()
         homepage.assert_url("Jackets for women")
         catalogSearch.assert_all_items_contain_keyword("Jacket")
-        # homepage.navigateToContactUs()
-        # contact_us = ContactUsPage(driver)
-        # contact_us.selectSubjectHeading("Customer service")
-        # contact_us.enterEmailAddress("test@test.com")
-        # contact_us.enterOrderReference("12345")
-        # contact_us.attachFile("data/testing.png")
-        # contact_us.enterMessage("Test message here")
-        # contact_us.sendForm()
-        # driver.implicitly_wait(5)
-        # self.driver.save_screenshot("../screenshots/" + "test_valid_contactUsFormSubmit.png")
-        # self.assertEqual("Your message has been successfully sent to our team.", contact_us.getMessageSent(),
-        # "Contact us message sent is not displayed")
-        # Navigate to ContactUs page
-        # homepage.navigateToContactUs()
-        # Validate Subject Header
-        # contact_us = ContactUsPage(driver)
-        # contact_us.enterEmailAddress("test@test.com")
-        # contact_us.enterOrderReference("12345")
-        # contact_us.enterMessage("Test")
-        # contact_us.sendForm()
-        # driver.implicitly_wait(5)
-        # self.assertEqual("Please select a subject from the list provided.", contact_us.getError(),
-        # "Subject Header is not validated")
-        # self.driver.save_screenshot("../screenshots/" + "test_invalid_subjectHeader.png")
-        # Validate Email address
-        # contact_us.selectSubjectHeading("Customer service")
-        # contact_us.enterEmailAddress("")
-        # contact_us.sendForm()
-        # driver.implicitly_wait(5)
-        # self.assertEqual("Invalid email address.", contact_us.getError(),
-        # "Email address is not validated")
-        # self.driver.save_screenshot("../screenshots/" + "test_empty_emailAddress.png")
-        # contact_us.enterEmailAddress("test.test")
-        # contact_us.sendForm()
-        # driver.implicitly_wait(5)
-        # self.assertEqual("Invalid email address.", contact_us.getError(),
-        # "Email address is not validated")
-        # self.driver.save_screenshot("../screenshots/" + "test_invalid_emailAddress.png")
-        # Validate Message text box
-        # contact_us.enterEmailAddress("test@test.com")
-        # contact_us.enterMessage("")
-        # contact_us.sendForm()
-        # driver.implicitly_wait(5)
-        # self.assertEqual("The message cannot be blank.", contact_us.getError(),
-        # "Message is not validated")
-        # self.driver.save_screenshot("../screenshots/" + "test_invalid_message.png")
-        # Validate Order reference
-        # contact_us.enterMessage("Test Message here")
-        # contact_us.enterOrderReference("")
-        # contact_us.sendForm()
-        # driver.implicitly_wait(5)
-        # self.driver.save_screenshot("../screenshots/" + "test_invalid_orderReference.png")
-        # self.assertEqual("The order reference cannot be blank.", contact_us.getError(),
-        # "Order Reference is not validated")
 
     # Verify search result using a valid keyword using the enter key
     def test_validateSearchFromEnterKey(self):
